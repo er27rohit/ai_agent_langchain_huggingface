eda9a554827cef0e7d207287983c2722164d3bfc
er27rohit <er27rohit@gmail.com>
2025-07-24 00:44:12 +0530

    Initialize

---
+++ main.py
@@ -0,0 +1,21 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+print("Token", os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+
+llm = HuggingFaceEndpoint(
+    # repo_id="HuggingFaceH4/zephyr-7b-beta", # Working but does need API TOKEN
+    repo_id="mradermacher/oh-dcft-v3.1-claude-3-5-sonnet-20241022-GGUF",
+    # repo_id="deepseek-ai/DeepSeek-R1-0528",  # Working but does need API TOKEN
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="auto",  # let Hugging Face choose the best provider for you
+)
+
+chat_model = ChatHuggingFace(llm=llm)
+response = chat_model.invoke("What is the capital of France? Reply in one word")
+print(response.content)
